31fe36d49ac6e14238d46a0f5bafed17a31c2454
lunnlew <4288315+lunnlew@users.noreply.github.com>
2021-12-06 16:29:58 +0800

    update elevate.ps1

--- bin/elevate/elevate.ps1
+++ bin/elevate/elevate.ps1
@@ -1,50 +1,51 @@
 $OFS = "`r`n"
-$ver = $host | select version
-if ($ver.Version.Major -gt 1)  {$Host.Runspace.ThreadOptions = "ReuseThread"}
+$ver = $host | Select-Object version
+if ($ver.Version.Major -gt 1) { $Host.Runspace.ThreadOptions = "ReuseThread" }
 
 # Verify that user running script is an administrator
-$Current=[Security.Principal.WindowsIdentity]::GetCurrent()
-If ((New-Object Security.Principal.WindowsPrincipal $Current).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) -eq $FALSE)
-{
-    #"`nERROR: You are NOT a local administrator.  Run this script after logging on with a local administrator account."
+$Current = [Security.Principal.WindowsIdentity]::GetCurrent()
+If ((New-Object Security.Principal.WindowsPrincipal $Current).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) -eq $FALSE) {
     # We are not running "as Administrator" - so relaunch as administrator
 
     # Create a new process object that starts PowerShell
     $ProcessInfo = new-object System.Diagnostics.ProcessStartInfo "cmd";
 
-    # Specify the current script path and name as a parameter
-    $output = New-TemporaryFile
-    # F
+    # Use temporary file to store the script back
+    $temp_file = New-TemporaryFile
+
+    # Format the command line parameters 
     $cmd_args = ($args -join ' ')
-    $ProcessInfo.Arguments = "/c " + "$cmd_args > " + $output;
+    $ProcessInfo.Arguments = "/c " + "$cmd_args > " + $temp_file;
+    
     # Indicate that the process should be elevated
     $ProcessInfo.Verb = "runas";
     
     $ProcessInfo.WindowStyle = "Hidden";
 
-    # # Require use shell execution policy
+    # Require use shell execution policy
     $ProcessInfo.UseShellExecute = $true;
 
     $ProcessInfo.WorkingDirectory = $PSScriptRoot;
 
     # Start the new process
-    try{
+    try {
         $process = [System.Diagnostics.Process]::Start($ProcessInfo);
         $process.WaitForExit()
-        $dd = Get-Content $output
-        Write-Output $dd
+        $content = Get-Content $temp_file
+        Write-Output $content
         Remove-Item -Force -ErrorAction SilentlyContinue $output
         exit 0
     }
-    catch [System.ComponentModel.Win32Exception]{
+    catch [System.ComponentModel.Win32Exception] {
         Write-Error "`nERROR: The user canceled the elevation prompt."
         exit 1
     }
-    catch{
+    catch {
         Write-Error "`nERROR: An error occurred while trying to elevate."
         exit 1
     }
-} else {
+}
+else {
     # We are running "as Administrator" - so continue with the script
     $cmd_args = ($args -join ' ')
     $cmd_result = (cmd /c $cmd_args)
