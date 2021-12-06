6978f304111dd89112cbe95e4c34273269a9db2a
lunnlew <4288315+lunnlew@users.noreply.github.com>
2021-12-06 16:00:05 +0800

    add off handle

--- types/index.d.ts
+++ types/index.d.ts
@@ -196,6 +196,13 @@ export default class WinswWrapper {
     ): this;
     /**
      * @param event keyof ServiceEvents
+     * @param listener 
+     */
+    off<U extends keyof ServiceEvents>(
+        event: U, listener: ServiceEvents[U]
+    ): this;
+    /**
+     * @param event keyof ServiceEvents
      * @param args 
      */
     emit<U extends keyof ServiceEvents>(
